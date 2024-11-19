3278be10b79d2aaa90ed771d4e23a6fd23ede410
brilliahib <brilliahib21@gmail.com>
2024-11-19 16:12:43 +0700

    feat: download document report for sales and purchase

--- src/components/organism/dashboard/DashboardSalesContent.tsx
+++ src/components/organism/dashboard/DashboardSalesContent.tsx
@@ -9,6 +9,10 @@ import { useGetAllSales } from "@/http/sales/get-all-sales";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useState } from "react";
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
+import { convertRupiah } from "@/utils/convertRupiah";
+import { Download, Plus } from "lucide-react";
 
 export default function DashboardSalesContent() {
   const { data: session, status } = useSession();
@@ -29,6 +33,42 @@ export default function DashboardSalesContent() {
     setDialogSalesOpen(true);
   };
 
+  const generatePDFReport = () => {
+    const doc = new jsPDF();
+
+    const now = new Date();
+    const currentDateTime = now.toLocaleString("id-ID", {
+      dateStyle: "short",
+      timeStyle: "short",
+    });
+
+    const pageWidth = doc.internal.pageSize.width;
+    doc.setFontSize(18);
+    const title = "Laporan Penjualan";
+    const titleWidth = doc.getTextWidth(title);
+    doc.text(title, (pageWidth - titleWidth) / 2, 20);
+
+    doc.setFontSize(10);
+    doc.text(currentDateTime, pageWidth - 10, 10, { align: "right" });
+
+    const tableData = filteredData.map((purchase, index) => [
+      index + 1,
+      purchase.medicine.name,
+      purchase.quantity,
+      convertRupiah(purchase.total_price),
+      new Date(purchase.created_at).toLocaleDateString(),
+    ]);
+
+    autoTable(doc, {
+      head: [["No", "Nama Obat", "Jumlah", "Harga", "Tanggal"]],
+      body: tableData,
+      startY: 30,
+      margin: { top: 10 },
+    });
+
+    doc.save("Laporan Penjualan.pdf");
+  };
+
   return (
     <>
       <div className="py-6 md:space-y-6 space-y-4">
@@ -38,7 +78,14 @@ export default function DashboardSalesContent() {
             className="min-w-[250px]"
             props="Cari penjualan..."
           />
-          <Button onClick={handleSalesDialogOpen}>Buat Penjualan</Button>
+          <div className="flex gap-4">
+            <Button onClick={generatePDFReport} variant={"outline"}>
+              <Download /> Unduh Laporan
+            </Button>
+            <Button onClick={handleSalesDialogOpen}>
+              <Plus /> Buat Penjualan
+            </Button>
+          </div>
         </div>
         <DataTable columns={salesColumns} data={filteredData} />
       </div>
--- src/components/organism/dashboard/admin/purchases/DashboardPurchasesAdminContent.tsx
+++ src/components/organism/dashboard/admin/purchases/DashboardPurchasesAdminContent.tsx
@@ -8,6 +8,10 @@ import { Button } from "@/components/ui/button";
 import { useGetAllPurchase } from "@/http/purchases/get-all-purchase";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
+import { convertRupiah } from "@/utils/convertRupiah";
+import { Download, Plus } from "lucide-react";
 
 export default function DashboardPurchasesAdminContent() {
   const { data: session, status } = useSession();
@@ -32,6 +36,42 @@ export default function DashboardPurchasesAdminContent() {
     setDialogCreatePurchaseOpen(true);
   };
 
+  const generatePDFReport = () => {
+    const doc = new jsPDF();
+
+    const now = new Date();
+    const currentDateTime = now.toLocaleString("id-ID", {
+      dateStyle: "short",
+      timeStyle: "short",
+    });
+
+    const pageWidth = doc.internal.pageSize.width;
+    doc.setFontSize(18);
+    const title = "Laporan Pembelian";
+    const titleWidth = doc.getTextWidth(title);
+    doc.text(title, (pageWidth - titleWidth) / 2, 20);
+
+    doc.setFontSize(10);
+    doc.text(currentDateTime, pageWidth - 10, 10, { align: "right" });
+
+    const tableData = filteredData.map((purchase, index) => [
+      index + 1,
+      purchase.medicine.name,
+      purchase.quantity,
+      convertRupiah(purchase.total_price),
+      new Date(purchase.created_at).toLocaleDateString(),
+    ]);
+
+    autoTable(doc, {
+      head: [["No", "Nama Obat", "Jumlah", "Harga", "Tanggal"]],
+      body: tableData,
+      startY: 30,
+      margin: { top: 10 },
+    });
+
+    doc.save("Laporan Pembelian.pdf");
+  };
+
   return (
     <>
       <div className="py-6 md:space-y-6 space-y-4">
@@ -41,7 +81,14 @@ export default function DashboardPurchasesAdminContent() {
             className="min-w-[250px]"
             props="Cari pembelian..."
           />
-          <Button onClick={handlePurchaseDialogOpen}>Buat Pembelian</Button>
+          <div className="flex gap-4">
+            <Button onClick={generatePDFReport} variant={"outline"}>
+              <Download /> Unduh Laporan
+            </Button>
+            <Button onClick={handlePurchaseDialogOpen}>
+              <Plus /> Buat Pembelian
+            </Button>
+          </div>
         </div>
         <DataTable columns={purchaseColumns} data={filteredData} />
       </div>
